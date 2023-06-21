c00992f586312cd56232b647198943cfcbda6c8d
ALESSANDRA RAYESFORTE <alerayes@hotmail.com>
2023-06-21 09:15:04 -0700

    Add small logo and large logo for responsiveness

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,31 +1,37 @@
-import Image from 'next/image'
+import { useState, useEffect } from 'react';
+import Image from 'next/image';
+import { Container } from '@/components/Container';
+import ColafoSm from '@/images/colafo-sm.png';
+import ColafoLg from '@/images/colafo-lg.png';
 
-import { Container } from '@/components/Container'
+export function Hero({ language }) {
+  const [isMobile, setIsMobile] = useState(false);
 
-export function Hero({language}) {
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 768); // Adjust the breakpoint as needed
+    };
+
+    handleResize(); // Set initial value
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   return (
-    <div className="relative mx-auto items-center max-w-7xl py-20 px-4 sm:px-6 lg:px-8">
+    <div className="relative mx-auto items-center max-w-7xl py-12 px-4 sm:px-6 lg:px-8">
       <Container className="relative">
-        <div className=" max-w-6xl lg:max-w-4xl">
-          <h1 className="font-display text-5xl font-bold tracking-tighter  sm:text-7xl">
-            <span className="sr-only">Congresso Latino-Americano de Fisioterapia em Oncologia - </span>
-            <span className='text-black'>
-              { language === 'pt' ?
-              'Congresso Latino-Americano de'
-              :
-              'Congreso Latinoamericano de'
-              }
-            </span> 
-            <span className='text-primary-green'> 
-              { language === 'pt' ?
-              ' Fisioterapia em Oncologia'
-              :
-              ' Fisioterapia en Oncología'
-              }
-            </span>
-          </h1>
+        <div className="max-w-4xl lg:max-w-7xl mx-auto md:mx-10">
+          <Image
+            src={isMobile ? ColafoSm : ColafoLg}
+            alt="colafo-logo"
+            unoptimized
+            priority={isMobile ? true : undefined}
+          />
         </div>
       </Container>
     </div>
-  )
+  );
 }
