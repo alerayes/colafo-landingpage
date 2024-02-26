5a61b784ebe70038e8023c941c796b84f30245b8
ALESSANDRA RAYESFORTE <alerayes@hotmail.com>
2024-02-25 20:18:49 -0800

    Last modifications to colafo: new expositores, apoiadores and parceiros

--- src/components/PatrocinadoresGerais.jsx
+++ src/components/PatrocinadoresGerais.jsx
@@ -3,6 +3,11 @@ import Image from 'next/image'
 import { Container } from '@/components/Container'
 import ZenFisio from '@/images/patrocinadores/zenfisio-logo.jpeg'
 import NegocioFisioterapia from '@/images/logos/negociofisioterapia-logo.png'
+import PitayasProductions from '@/images/logos/pitayaproductions-logo.jpeg'
+import Outback from '@/images/logos/outback-logo.jpeg'
+import Abbraccio from '@/images/logos/abbraccio-logo.jpeg'
+
+
 
 export function PatrocinadoresGerais() {
   return (
@@ -25,6 +30,15 @@ export function PatrocinadoresGerais() {
               </a>              
               <a href="https://www.instagram.com/negociofisioterapia/?hl=en" className='mb-8 mt-4 md:ml-10'>
                   <Image src={NegocioFisioterapia} alt={'negociofisioterapia-logo'} unoptimized width={250} height={10}/>
+              </a>
+              <a href="https://www.instagram.com/outbackbrasil/?hl=en" className='mb-8 mt-4 md:ml-10'>
+                  <Image src={Outback} alt={'outback-logo'} unoptimized width={250} height={10}/>
+              </a>
+              <a href="https://www.instagram.com/abbracciorestaurante/?hl=en" className='mb-8 mt-4 md:ml-10'>
+                  <Image src={Abbraccio} alt={'abbraccio-logo'} unoptimized width={250} height={10}/>
+              </a>
+              <a href="https://www.instagram.com/pitayasproductionsbr/?hl=en" className='mb-8 mt-4 md:ml-10'>
+                  <Image src={PitayasProductions} alt={'pitayasproductions-logo'} unoptimized width={250} height={10}/>
               </a>
             </div>
         </div>
